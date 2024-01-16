f4559d6cd6b2e004ba7731d81ba9861a7cf44bdd
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2024-01-16 23:58:18 +0700

    feat: react/jsx-key in TopGenres

--- components/Spotify/TopGenres.tsx
+++ components/Spotify/TopGenres.tsx
@@ -9,8 +9,8 @@ const SpotifyTopGenresGridElement = ({ genresRanking } : {genresRanking: Genre[]
     return (
         <div className="grid justify-center gap-6 mt-10 sm:mt-12 md:mt-16 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
             {genresRanking.map((data, i) => (
-                <Link href={`https://google.com/search?q=${data.genre}%20genre`} target="_blank" rel="noreferer">
-                    <div key={i} className="card bg-primary z-0 w-96 h-96 shadow-xl image-full">
+                <Link key={i} href={`https://google.com/search?q=${data.genre}%20genre`} target="_blank" rel="noreferer">
+                    <div className="card bg-primary z-0 w-96 h-96 shadow-xl image-full">
                         <div className="card-body">
                             <h2 className="card-title text-3xl">#{(i + 1).toString()}</h2>
                             <div className="flex-shrink">
